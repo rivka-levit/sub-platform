7238b2016167031081760cf9b1e5e2b8b077936b
rivka-yuliia <juliacreadora@gmail.com>
2025-02-27 16:02:59 +0200

    custom user admin

--- app/account/admin.py
+++ app/account/admin.py
@@ -1,5 +1,49 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from django.utils.translation import gettext_lazy as _
 
 from account.models import CustomUser
 
-admin.site.register(CustomUser)
+
+class CustomUserAdmin(UserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'full_name']
+    fieldsets = (
+        (None, {'fields': (
+            'email',
+            'password',
+            'first_name',
+            'last_name',
+            'is_writer'
+        )}),
+        (
+            _('Permissions'), {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('date_joined', 'last_login',)}),
+    )
+    readonly_fields = ['date_joined', 'last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'first_name',
+                'last_name',
+                'is_active',
+                'is_staff',
+                'is_superuser'
+            ),
+        }),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
