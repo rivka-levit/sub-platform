6f886de3dfefa3f94f405b71436263d015e28246
rivka-yuliia <juliacreadora@gmail.com>
2025-03-04 16:43:04 +0200

    article model, tests

--- app/writer/models.py
+++ app/writer/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
 
-# Create your models here.
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=255, blank=True)
+    content = models.TextField(max_length=10000, blank=True)
+    author = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    date_posted = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    is_premium = models.BooleanField(
+        default=False,
+        verbose_name="Is it a premium article?"
+    )
+
+    class Meta:
+        verbose_name = 'article'
+        verbose_name_plural = 'articles'
+        unique_together = ('slug', 'author')
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Article, self).save(*args, **kwargs)
---
+++ app/writer/tests/test_writer_models.py
@@ -0,0 +1,78 @@
+"""
+Tests for writer app models.
+Command: pytest writer/tests/test_writer_models.py --cov=writer --cov-report term-missing:skip-covered
+"""
+
+import pytest
+
+from django.utils.text import slugify
+from django.db.utils import IntegrityError
+
+from writer.models import Article
+
+pytestmark = pytest.mark.django_db
+
+
+def test_create_article_pass(user_writer):
+    """Test creating an article successfully."""
+
+    data = {
+        'title': 'Test title',
+        'content': 'Test content',
+        'author': user_writer,
+    }
+
+    article = Article.objects.create(**data)
+
+    assert article.title == data['title']
+    assert article.content == data['content']
+    assert article.author == user_writer
+    assert article.slug == slugify(data['title'])
+    assert str(article) == article.title
+
+
+def test_create_article_with_repeating_slug_same_user_fails(user_writer):
+    """Test creating an article of the same user with repeating slug fails."""
+
+    data = {
+        'title': 'Test title',
+        'content': 'Test content',
+        'author': user_writer,
+    }
+
+    Article.objects.create(**data)
+
+    with pytest.raises(IntegrityError):
+        Article.objects.create(**data)
+
+def test_create_article_with_repeating_slug_different_users_pass(user):
+    """Test creating an article with the same slug to different users pass."""
+
+    writer1 = user(email='writer1@example.com', is_writer=True)
+    writer2 = user(email='writer2@example.com', is_writer=True)
+
+    payload1 = {'title': 'Test title 1', 'slug': 'repeating-slug',
+                'content': 'Test content 1', 'author': writer1}
+    payload2 = {'title': 'Test title 2', 'slug': 'repeating-slug',
+                'content': 'Test content 2', 'author': writer2}
+
+    Article.objects.create(**payload1)
+    Article.objects.create(**payload2)
+
+    query_set = Article.objects.filter(slug='repeating-slug')
+
+    assert query_set.count() == 2
+
+def test_create_article_with_custom_slug_success(user_writer):
+    """Test creating an article with custom slug successfully."""
+
+    data = {
+        'title': 'Test title',
+        'content': 'Test content',
+        'slug': 'custom-title-slug',
+        'author': user_writer,
+    }
+
+    article = Article.objects.create(**data)
+    assert article.title == data['title']
+    assert article.slug == data['slug']
