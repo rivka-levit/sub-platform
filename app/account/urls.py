4cff24d1523e1b43a2db3dcf49c8d54ab4367566
rivka-yuliia <juliacreadora@gmail.com>
2025-04-08 18:24:23 +0300

    reset password urls

--- app/account/urls.py
+++ app/account/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from account import views
 
@@ -8,4 +9,34 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('account/', views.AccountView.as_view(), name='account'),
     path('delete-account', views.delete_account, name='delete_account'),
+
+    # Password management
+
+    # Enter email to receive password reset link
+    path(
+        'reset-password/',
+        auth_views.PasswordResetView.as_view(),
+        name='reset_password'
+    ),
+
+    # Email sent successfully page
+    path(
+        'reset-password/sent/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done'
+    ),
+
+    # Change password link
+    path(
+        'reset-password/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'
+    ),
+
+    # Success page password has been changed
+    path(
+        'reset-password/complete/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete'
+    ),
 ]
